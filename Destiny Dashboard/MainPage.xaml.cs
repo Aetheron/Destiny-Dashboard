a0f9b533abf3c1dfd508b7c3fd5b6933228cb423
Robert VanLonkhuyzen <hey.bob.oh@gmail.com>
2020-11-03 15:40:43 -0500

    * Initial commit. Add auth button to show web view

---
+++ Destiny Dashboard/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Destiny_Dashboard
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Authenticate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Auth_Window.IsOpen) { Auth_Window.IsOpen = true; }
+        }
+    }
+}
